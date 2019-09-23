ba208305fb8cd365011133cc627f3a396872b100
Steven Hunal <stevehunal@gmail.com>
2019-09-23 19:48:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,28 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+  enable :sessions # session = {}
+  set :session_secret, "secret"
+end
+
+  get "/" do 
+    erb :index
+  end
+  
+  post "/checkout" do 
+    
+    session[:item]= params[:item]
+    
+    redirect "/response"
+    
+  end 
+  
+  get "/response" do
+    @item = session[:item]
+    
+    erb :response
+  end
+  
 end
